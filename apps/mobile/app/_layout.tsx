e7adc525b15f92e71f05e444667716ac0937fe6f
Matheus Timbó Pereira <matheusfild4@hotmail.com>
2026-09-10 07:07:35 -0300

    fix(mobile): close the theme provider

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout() {
             <ThemeSceneProvider>
               <RootNavigator />
             </ThemeSceneProvider>
+          </ThemeProvider>
         </AppProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
